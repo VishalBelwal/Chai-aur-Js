164312dcd162b32ced0d1556c876daf1eaa68b38
VishalBelwal <155064966+VishalBelwal@users.noreply.github.com>
2024-04-20 12:52:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all js code as newer version
+
+// alert("Hello") we are using node not browser isme alag tarika hai ise run karne ka
+console.log(3 + 3)
+console.log("Vishal")
+
+let name = "Vishal"  //string datatype
+let age = 18   //number datatype
+let isLoggedin = false   //boolean datatype
+
+//DATATYPES
+/*
+Primitive Datatypes
+
+1--number => till 2 to the power 53
+2--bigInt
+3---string => ""
+4--boolean => true/false
+5--null => standalone value ye ek empty value hai  iska matlab wom undefined nahi hai wo khali hai
+6--Undefined => jab koi variable declare kara hai but value assing nahi kari
+7---symbol => uniqueness found karne ke liye use hota hai
+*/
+
+console.log(typeof null)  //undefined ek type hai but null ek object hai  output-->object
+
+/*
+OBJECT
+*/
