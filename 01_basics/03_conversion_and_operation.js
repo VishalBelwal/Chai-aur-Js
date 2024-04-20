47ad8dab29f78153efe9477b17addad1db084be1
VishalBelwal <155064966+VishalBelwal@users.noreply.github.com>
2024-04-20 19:09:46 +0000

    datatypes_2

--- 01_basics/03_conversion_and_operation.js
+++ 01_basics/03_conversion_and_operation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/*let score = "33abc"
 
 console.log(score);
 console.log(typeof score);
@@ -6,29 +6,52 @@ console.log(typeof score);
 let valueInNumber = Number(score) //ab ye jo score hai wo number ki form me print hokar aayega
 
 console.log(valueInNumber);
-console.log(typeof valueInNumber);
+console.log(typeof valueInNumber);*/
 
 //agar null hota to 0 output aayega and undefined hoga to wapas se nan aayega
-/*
-"33" => 33
-"33abc" => NaN
-true => 1 and false => 0
-*/
 
-let isLoggedIn = 1  
+// "33" => 33
+// "33abc" => NaN
+// true => 1 and false => 0
+
+
+/*let isLoggedIn = 1  
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-
-/*
-1 => true; 0 => false
-"" => false
-"vishal" => true
-*/
+console.log(booleanIsLoggedIn);*/
 
 
-let someNumber = 33
+// 1 => true; 0 => false
+// "" => false
+// "vishal" => true
+
+
+
+/*let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
+
+
+
+// **********************************Operations*****************************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Vishal"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1 + 2 + "2");  //konsi value phele aa rahi hai wo hi prefer hogi
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
