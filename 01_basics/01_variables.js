165d01e99642fc337b145797fcb12a2d61922500
VishalBelwal <155064966+VishalBelwal@users.noreply.github.com>
2024-04-20 12:31:51 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1445533
+let accoutEmail = "vishal27@gmail.com"
+var accoutPassword = "1234556"
+accountCity = "Delhi"
+let accountSatate;  //undefined value
+
+
+
+
+accoutEmail = "vb@vb.com"
+accoutPassword = "3242423"
+accountCity = "Bengalore"
+
+console.log(accountId);
+
+/*
+prefer not to use var because of issue in block and functional scope
+*/
+
+console.table([accountId, accoutEmail, accoutPassword, accountCity, accountSatate])
