e0c44e6a657bd2f869cf54a603e5c6facc06e431
VishalBelwal <155064966+VishalBelwal@users.noreply.github.com>
2024-04-20 19:50:39 +0000

    comparison modified

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -19,3 +19,30 @@ console.log(null >= 0);
 //agar in sabko undefined ke sath bhi karenge to value false hi milegi
 
 console.log("2" === 2)  //yha par value automatcally number me convert nahi ho rahii
+
+
+// ****************************************************
+/*Memory 2 type ki hoti hai stack and heap
+stack was used in primitive datatypes => (ye original value ko change nahi karta)iska matlab ye hai ki jo bhi hamne variable declare kara uska ek copy milti hai (ek ke upar ek value store and change hoti rheti hai)and
+heap was used in non primitive datatypes => iska matlab ye hai ki jo bhi ham change karenge wo original value me change hoga
+*/
+
+let myInstaName = "_vishall.27_"
+
+let anotherName = myInstaName
+anotherName = "chaiaurcode"
+console.log(myInstaName);
+console.log(anotherName);
+
+let userOne = {
+    email : "vb@gmail.com",
+    upiId : "321654@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user@google.com"
+
+//dono ke andar value same ayegi
+console.log(userOne.email);
+console.log(userTwo.email);
