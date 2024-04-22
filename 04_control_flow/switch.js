d49dece051564a38b2afa4ec0ab8bb1204d2c9ca
VishalBelwal <155064966+VishalBelwal@users.noreply.github.com>
2024-04-22 14:29:58 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+
+//key wo value jiske liye hame condition check karne hai
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+//agar break na ho to condition ke satisfy hone ke baad saara code run hoga except default
+
