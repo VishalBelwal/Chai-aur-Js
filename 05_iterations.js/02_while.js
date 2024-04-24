c21c193e0a56ef9365cc34d80e852e8ca9cc0889
VishalBelwal <155064966+VishalBelwal@users.noreply.github.com>
2024-04-24 07:30:12 +0000

    while

---
+++ 05_iterations.js/02_while.js
@@ -0,0 +1,23 @@
+
+let index = 0
+
+while (index <= 5) {
+    // console.log("Hello");
+    index = index + 2
+}
+
+let myArr = ['flash', 'batman', 'superman']
+
+let arr = 0
+while (arr < myArr.length) {
+    // console.log(`Value is ${myArr[arr]}`);
+    arr++
+}
+
+/*do-while loop*/
+let score = 0
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while(score <= 10)
